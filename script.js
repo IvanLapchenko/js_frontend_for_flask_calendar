1b7b8db4ba52c55dae2f98da282c02bbb10d5181
LapchenkoIvan <lapchenko.vanya@gmail.com>
2023-03-06 11:19:04 +0200

    Add routes and created handlers for routes

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 window.onload = (event) => {
-
     const routes = [
       { path: '/', handler: homeHandler },
-      { path: '/login', handler: loginHandler },
-      { path: '/register', handler: registerHandler },
-      { path: '/home', handler: homeHandler },
+      { path: '/login.html', handler: loginHandler },
+      { path: '/signup.html', handler: signupHandler },
+      { path: '/index.html', handler: homeHandler },
     ];
 
+    handleRoutes()
 
-    const form = document.querySelector('#add-new-event');
 
-    form.addEventListener('submit', (event) => {
-      event.preventDefault();
+
+    function loginHandler () {
+        const loginForm = document.querySelector('#login-form');
+        const urlLogin = 'http://127.0.0.1:5000/login';
+        console.log('a')
+        loginForm.addEventListener('submit', (event) => {
+                event.preventDefault();
+                console.log(event.target);
+                sendRequestToServer(event.target, urlLogin)
+                .then(data => {
+                    localStorage.setItem('token', data.token);
+                    console.log(data);
+                })
+        })
+    }
+
+    function signupHandler () {
+        console.log("Signup")
+    }
+
+    function homeHandler () {
+        console.log("Home")
+    }
+
+    function sendRequestToServer(form, url) {
 
       const formData = new FormData(form);
       const data = {};
@@ -20,31 +42,43 @@ window.onload = (event) => {
         data[key] = value;
       }
 
-      fetch('http://127.0.0.1:5000/create_event', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      })
-        .then(data => console.log(data))
-        .catch(error => console.error(error));
-    });
-
-
-
-
-    const today = new Date().toISOString();
-
-    const apiUrl = `http://127.0.0.1:5000/get_events_by/${today}`;
-
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-      })
-      .catch(error => {
-        console.error('Error:', error);
+      return new Promise((resolve, reject) => {
+          fetch(url, {
+              method: "POST",
+              headers: { "Content-type": "application/json" },
+              body: JSON.stringify(data),
+            })
+            .then((response) => response.json())
+            .then((data) => { resolve(data) })
+            .catch((error) => {
+              console.error("Помилка:", error);
+              reject(error);
+            });
       });
+    }
+
+
+    function handleRoutes () {
+        const currentPath = window.location.pathname;
+        const routeData = routes.find(route => route.path === currentPath);
+        if (routeData) {
+            routeData.handler();
+           }
+        else { homeHandler() }
+    }
+
+
+//    const today = new Date().toISOString();
+//
+//    const apiUrl = `http://127.0.0.1:5000/get_events_by/${today}`;
+//
+//    fetch(apiUrl)
+//      .then(response => response.json())
+//      .then(data => {
+//        console.log(data);
+//      })
+//      .catch(error => {
+//        console.error('Error:', error);
+//      });
 
 }
