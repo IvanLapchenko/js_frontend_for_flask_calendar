ff4c3248ed4f4d3ad4151587cb21e720495147dd
LapchenkoIvan <lapchenko.vanya@gmail.com>
2023-03-07 01:08:55 +0200

    Implement loginization

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 window.onload = (event) => {
+
     const routes = [
       { path: '/', handler: homeHandler },
       { path: '/login.html', handler: loginHandler },
@@ -13,6 +14,7 @@ window.onload = (event) => {
     function loginHandler () {
         const loginForm = document.querySelector('#login-form');
         const urlLogin = 'http://127.0.0.1:5000/login';
+
         loginForm.addEventListener('submit', (event) => {
                 event.preventDefault();
                 sendRequestToServer(event.target, urlLogin)
@@ -28,39 +30,51 @@ window.onload = (event) => {
     }
 
     function homeHandler () {
+        const logoutButton = document.querySelector('#logout');
         const eventForm = document.querySelector('#add-new-event');
         const urlEvent = 'http://127.0.0.1:5000/create_event';
+        const today = new Date().toISOString();
+
+        getEventsByDate(today)
+        .then(data => console.log(data))
+
+
         eventForm.addEventListener('submit', (event) => {
                 event.preventDefault();
                 sendRequestToServer(event.target, urlEvent)
                 .then(data =>  console.log(data))
         })
 
-        const today = new Date().toISOString();
-        getEventsByDate(today)
-        .then(data => console.log(data))
+        logoutButton.addEventListener('click', (event) => {
+            logout();
+        })
     }
 
 
     function logout () {
-        localStorage.removeItem('jwt');
-        location.replace('/index.html');
+        localStorage.removeItem('token');
+        location.replace('/login.html');
     }
 
 
     function sendRequestToServer(form, url) {
-
       const formData = new FormData(form);
       const data = {};
+      const token = localStorage.getItem('token');
+      let headersData = { "Content-type": "application/json" };
 
       for (const [key, value] of formData.entries()) {
         data[key] = value;
       }
+      if (token)
+        headersData.Authorization = `Bearer ${token}`;
+
 
       return new Promise((resolve, reject) => {
+
           fetch(url, {
               method: "POST",
-              headers: { "Content-type": "application/json" },
+              headers: headersData,
               body: JSON.stringify(data),
             })
             .then((response) => response.json())
@@ -85,22 +99,16 @@ window.onload = (event) => {
 
     function getEventsByDate (date) {
         const token = localStorage.getItem('token');
-        console.log(token)
         const apiUrl = `http://127.0.0.1:5000/get_events_by/${date}`;
-
-        const myHeaders = new Headers();
-        myHeaders.append(Authorization, `Bearer ${token}`);
 
         return fetch(apiUrl, {
             method: "GET",
-                headers: {
-                  Authorization: `Bearer ${localStorage.getItem('jwt')}`,
-                }
+                headers: { Authorization: `Bearer ${token}`}
           })
           .then(response => response.json())
           .catch(error => {
             console.error('Error:', error);
           });
-        }
+    }
 
 }
